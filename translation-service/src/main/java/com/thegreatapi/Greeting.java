f1afe42026916d5b564cd07820560e6f2758ec0d
Helber Belmiro <helber.belmiro@gmail.com>
2022-05-13 18:03:53 -0300

    Created translation-service
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ translation-service/src/main/java/com/thegreatapi/Greeting.java
@@ -0,0 +1,14 @@
+package com.thegreatapi;
+
+public class Greeting {
+
+    private final String greeting;
+
+    public Greeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
---
+++ translation-service/src/main/java/com/thegreatapi/TranslateResource.java
@@ -0,0 +1,25 @@
+package com.thegreatapi;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/translate")
+public class TranslateResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Greeting translate(Data data) {
+        switch (data.getLanguage()) {
+            case "Portuguese":
+                return new Greeting("Saudações do Serverless Workflow, " + data.getName() + "!");
+            case "Spanish":
+                return new Greeting("Saludos desde Serverless Workflow, " + data.getName() + "!");
+            default:
+                return new Greeting("Greetings from Serverless Workflow, " + data.getName() + "!");
+        }
+    }
+}
