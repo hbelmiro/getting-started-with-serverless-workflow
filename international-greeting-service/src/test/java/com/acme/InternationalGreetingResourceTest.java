8876e590218a4216fdfbf6fa0c9cf7cd4309c2fb
Helber Belmiro <helber.belmiro@gmail.com>
2023-05-16 18:34:10 -0300

    Added tests to international-greeting-service
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ international-greeting-service/src/test/java/com/acme/InternationalGreetingResourceTest.java
@@ -0,0 +1,34 @@
+package com.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.inject.Inject;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@QuarkusTest
+class InternationalGreetingResourceTest {
+
+    @Inject
+    InternationalGreetingResource resource;
+
+    @ParameterizedTest
+    @MethodSource("getGreetingSource")
+    void getGreeting(String language, String name, String expectedMessage) {
+        var data = new Data(language, name);
+        assertThat(resource.getGreeting(data).getGreeting()).isEqualTo(expectedMessage);
+    }
+
+    public static Stream<Arguments> getGreetingSource() {
+        return Stream.of(
+                Arguments.of("Portuguese", "Helber", "Saudações do Serverless Workflow, Helber!"),
+                Arguments.of("Spanish", "Helber", "Saludos desde Serverless Workflow, Helber!"),
+                Arguments.of("English", "Helber", "Greetings from Serverless Workflow, Helber!")
+        );
+    }
+}
